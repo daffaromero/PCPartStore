ebe86823afea59897ac71e58e87727b1ed197cf3
daffaromero <70407002+daffaromero@users.noreply.github.com>
2021-04-10 10:07:13 +0700

    Delete dbo.BillTable.sql

--- dbo.BillTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[BillTable] (
-    [BillID]      INT           NOT NULL,
-    [SellerName]  NVARCHAR (25) NOT NULL,
-    [BillDate]    NVARCHAR (25) NOT NULL,
-    [TotalAmount] INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([BillID] ASC)
-);
-
