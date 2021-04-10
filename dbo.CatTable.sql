eedbbfa79c85c3e1feaabea0fa877747b361c188
daffaromero <70407002+daffaromero@users.noreply.github.com>
2021-04-10 10:07:21 +0700

    Delete dbo.CatTable.sql

--- dbo.CatTable.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[CatTable] (
-    [CatID]   INT        NOT NULL,
-    [CatName] NCHAR (10) NOT NULL,
-    [CatDesc] NCHAR (25) NOT NULL,
-    PRIMARY KEY CLUSTERED ([CatID] ASC)
-);
-
