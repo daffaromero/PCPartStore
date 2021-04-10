e2466e142a787e54defab69c808417b6678d1bf9
daffaromero <70407002+daffaromero@users.noreply.github.com>
2021-04-10 10:07:33 +0700

    Delete dbo.SellerTable.sql

--- dbo.SellerTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[SellerTable] (
-    [SellerID]    INT           NOT NULL,
-    [SellerPwd]   NVARCHAR (30) NOT NULL,
-    [SellerName]  NVARCHAR (20) NOT NULL,
-    [SellerPhone] NVARCHAR (20) NOT NULL,
-    PRIMARY KEY CLUSTERED ([SellerID] ASC)
-);
-
